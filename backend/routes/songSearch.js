914155e4aef3b48ea4958883ed566364b560515d
michaelgchen <michaelguanyichen@gmail.com>
2021-12-12 13:59:05 -0600

    Initial Database Setup
    -Set up MongoDB atlas cluster
    -Modified SongSearch schema
    -Wrote post request for songSearch for purposes of testing

--- backend/routes/songSearch.js
+++ backend/routes/songSearch.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import SongSearch from '../schemas/SongSearch.js';
 
 const router = Router();
 
@@ -8,5 +9,30 @@ const router = Router();
  Input: Artist Name, Song Name
  Output: SongSearch object: { Song image, Map of words in song -> Count of word }
 */
+
+router.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+
+router.post('/', function (req, res) {
+    var song = new SongSearch({
+        songImage: req.body.songImage,
+        wordCounts: req.body.wordCounts,
+    });
+       
+    song.save()
+        .then(item =>{
+            return res.status(201).send({
+                message: 'Successfully Created Song',
+                data: item
+            }); 
+        })
+        .catch(err => {
+            res.status(400).send("Unable to add song to the database")
+        });  
+});
+
+
 
 export default router;
--- backend/schemas/SavedSongs.js
+++ backend/schemas/SavedSongs.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
-import SongSearch from './SongSearch';
+//import SongSearch from './SongSearch';
+import SongSearch from '../schemas/SongSearch.js';
+
 
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
 const SavedSongsSchema = new Schema({
   userId /*or email*/: String /*or Number*/,
-  savedSongs: [SongSearch],
+  savedSongs: [SongSearch], 
 });
 
-const SongSearch = model('songSearch', SongSearchSchema);
+const SavedSongs = model('SavedSongs', SavedSongsSchema);
 
-export default SongSearch;
+export default SavedSongs;
