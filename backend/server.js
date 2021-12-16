0dff44a4555561f7cb27f461222e4df030424fc2
DS <ds>
2021-12-15 18:38:27 -0600

    updated server.js for prod

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 import rootRouter from './routes/rootRouter.js';
 
@@ -13,9 +16,19 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log('Connected to MongoDB Database.'))
-  .catch(err => console.log(err));
+  .catch(error => console.log(error));
 
 app.use('/', rootRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/build'));
+
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  app.get('/*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
